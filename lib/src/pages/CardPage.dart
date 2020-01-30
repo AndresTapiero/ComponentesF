2db402526ea5d61b3ae44752f2034d4143e62c03
AndresTapiero72 <andres.tapiero@grability.com>
2020-01-29 22:11:41 -0500

    cardPage and loading gif

--- lib/src/pages/CardPage.dart
+++ lib/src/pages/CardPage.dart
@@ -7,6 +7,68 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2(),
+
+        ],
+      ),
+    );
+  }
+
+  _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('Soy el titulo de esta tarjeta'),
+            subtitle: Text('Aqui estamos con la descripción con la tarjeta para que ustedes tengan una idea de lo que quiero mostrarles!'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text ('Cancelar'),
+                onPressed: (){},
+              ),
+              FlatButton(
+                child: Text ('Ok'),
+                onPressed: (){},
+              ),
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/3/35/Neckertal_20150527-6384.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration ( milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+
+
+/*          Image(
+            image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/3/35/Neckertal_20150527-6384.jpg'),
+          ),*/
+          
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('No Tengo idea de que poner'))
+        ],
+      ),
     );
   }
 }
