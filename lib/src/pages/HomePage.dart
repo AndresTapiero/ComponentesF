e298694ab15b22da1c2ea1c46668f2e3fce716ba
AndresTapiero72 <andres.tapiero@grability.com>
2020-01-29 22:11:14 -0500

    change name class

---
+++ lib/src/pages/HomePage.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:componentes_app/src/Utils/iconoStringUtil.dart';
+import 'package:componentes_app/src/providers/menu_provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: ( context, AsyncSnapshot<List<dynamic>> snapshot){
+
+        return ListView(
+          children: _lisItems(snapshot.data, context),
+        );
+
+      },
+    );
+
+/*    print (menuProvider.opciones);
+    return ListView(
+      children: _lisItems(),
+    );*/
+  }
+
+  List<Widget> _lisItems(List<dynamic> data, BuildContext context) {
+
+    final List<Widget> opciones = [];
+
+    data.forEach((opt){
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(Icons.keyboard_arrow_down),
+        onTap: (){
+          Navigator.pushNamed(context, opt['ruta']);
+
+/*          final route = MaterialPageRoute(
+              builder: (context) => AlertPage()
+          );
+          Navigator.push(context, route);*/
+        },
+
+      );
+      opciones..add(widgetTemp)
+        ..add(Divider());
+    });
+    return opciones;
+  }
+}
