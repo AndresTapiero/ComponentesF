fa4c95926bd544ddad71738480d5d30c6e637623
AndresTapiero72 <andres.tapiero@grability.com>
2020-01-30 21:51:37 -0500

    create Route dart

--- lib/src/routes/Routes.dart
+++ lib/src/routes/Routes.dart
@@ -1,15 +1,14 @@
 import 'package:componentes_app/src/pages/CardPage.dart';
+import 'package:componentes_app/src/pages/HomePage.dart';
 import 'package:componentes_app/src/pages/alertPage.dart';
 import 'package:componentes_app/src/pages/avatarPage.dart';
-import 'package:componentes_app/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-Map<String, WidgetBuilder> getApplicationRoutes(){
+Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
-    'card':(BuildContext context) => CardPage(),
+    'card': (BuildContext context) => CardPage(),
   };
 }
-
