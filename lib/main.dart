792eaf6c0317f80641e9840357d5a324e060f77c
AndresTapiero <andrestapiero72@gmail.com>
2020-02-06 23:45:42 -0500

    location dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:componentes_app/src/routes/Routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:componentes_app/src/pages/alertPage.dart';
 
 void main () => runApp(MyApp());
@@ -12,7 +13,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes',
       debugShowCheckedModeBanner: false,
-      //home: HomePage(),
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'), // English
+        const Locale('es'), // SSpanish
+
+      ],
       initialRoute: '/',
       routes: getApplicationRoutes(),
       onGenerateRoute: (RouteSettings settings){
